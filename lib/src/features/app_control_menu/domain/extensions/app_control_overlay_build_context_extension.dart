78ee45b4fa9320564a928d36a0761809deae8987
Flutter jobb <maher@flutterjobb.se>
2025-02-27 13:18:15 +0100

    fix: setup starter project

--- lib/src/features/app_control_menu/domain/extensions/app_control_overlay_build_context_extension.dart
+++ lib/src/features/app_control_menu/domain/extensions/app_control_overlay_build_context_extension.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:overlays_workshop/src/features/swipe_down_menu/presentation/view_notifier/swipe_down_menu_view_notifier.dart';
 
 extension AppControlOverlayBuildContextX on BuildContext {
-  SwipeDownMenuViewNotifier get behaviourCubit =>
-      read<SwipeDownMenuViewNotifier>();
-
   Size get screenSize => MediaQuery.of(this).size;
   double get screenWidth => screenSize.width;
   double get screenHeight => screenSize.height;
--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:overlays_workshop/src/features/home/domain/constants/home_panorama_assets.dart';
 import 'package:overlays_workshop/src/features/home/presentation/widgets/panorama_widget.dart';
-import 'package:overlay_plus/overlay_plus.dart';
-import 'package:overlays_workshop/src/features/swipe_down_menu/presentation/view/widgets/swipe_down_menu.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,20 +12,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final _overlayController = OverlayPlusController();
-
   @override
   Widget build(BuildContext context) {
-    return SwipeDownMenu(
-      overlayController: _overlayController,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: PanoramaWidget(assets: kHomePanoramaAssets),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: PanoramaWidget(assets: kHomePanoramaAssets),
       ),
     );
   }
